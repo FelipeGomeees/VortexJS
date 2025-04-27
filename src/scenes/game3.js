82524fda846bb0f6151682bad2e1448ddf5087b9
FelipeGomeees <felipegomes.dsn@gmail.com>
2025-04-27 17:28:57 -0300

    refactor: current approach refactor to the test code to the current way of doing things

--- src/scenes/game3.js
+++ src/scenes/game3.js
@@ -1,13 +1,14 @@
 import { RenderService, Scene, SceneControl, SpriteControl, Vector2 } from "../modules/vortex.js";
 import { player, playerMovement } from "../entities/game3/player.js";
-import { enemy, enemyAI } from "../entities/game3/enemy.js";
+import { newEnemy, enemyAI } from "../entities/game3/enemy.js";
 import { checkCollision } from "../helpers/checkCollision.js";
 import { AudioControl, Entity } from "../modules/vortex.js";
 import { target } from "../entities/game3/target.js";
 
 const patrolScene = new Scene("enemyTest", "#eee", true);
 
-let thisPlayer, thisEnemy, thisTarget;
+let thisPlayer, thisTarget;
+const thisEnemies = []
 
 patrolScene.setSetup(() => {
     AudioControl.Load('bounce', 'src/assets/audio/bounce.wav');
@@ -19,30 +20,39 @@ patrolScene.setSetup(() => {
     SpriteControl.Load("target", "src/assets/img/checkpoint.png");
 
     thisPlayer = patrolScene.AddEntity({ tag: 'player', obj: player, canMove: true, attachedSprite: 'player' });
-    thisEnemy = patrolScene.AddEntity({ tag: 'enemy', obj: enemy, canMove: true, attachedSprite: 'enemy' });
+
+    thisEnemies.push(patrolScene.AddEntity({ tag: 'enemy', obj: newEnemy(), canMove: true, attachedSprite: 'enemy' }));
+
     thisTarget = patrolScene.AddEntity({ tag: 'target', obj: target, canMove: false, attachedSprite: 'target' });
 
-    patrolScene.UpdateEntity('target', {
+    console.log(newEnemy());
+    console.log(newEnemy());
+    patrolScene.UpdateEntityByTag('target', {
         position: new Vector2(RenderService.vw - 50, RenderService.vh - 50)
     });
 });
 
 patrolScene.setLoop(() => {
-    patrolScene.UpdateEntity('player', playerMovement(thisPlayer.obj));
-    patrolScene.UpdateEntity('enemy', enemyAI(thisEnemy.obj));
+    patrolScene.UpdateEntityByTag('player', playerMovement(thisPlayer.obj));
 
-    if (checkCollision(player, enemy)) {
-        AudioControl.Play('defeat');
-        console.log("💥 Player hit the enemy!");
-        player.position = new Vector2(50, 50);
-        // patrolScene.AddEntity({ tag: 'enemy', obj: enemy, canMove: true });
+    for (const thisEnemy of thisEnemies) {
+        patrolScene.UpdateEntityByTag('enemy', enemyAI(thisEnemy.obj));
     }
+    
 
-    if (checkCollision(player, target)) {
+    // if (checkCollision(player, enemy)) {
+    //     AudioControl.Play('defeat');
+    //     console.log("💥 Player hit the enemy!");
+    //     player.position = new Vector2(50, 50);
+    //     // patrolScene.AddEntity({ tag: 'enemy', obj: enemy, canMove: true });
+    // }
+
+    if (checkCollision(thisPlayer.obj, thisTarget.obj)) {
         AudioControl.Play('victory');
         console.log("💥 Player hit the target!");
-        player.position = new Vector2(50, 50);
-        patrolScene.AddEntity({ tag: 'enemy', obj: enemy, canMove: true });
+        thisPlayer.obj.position = new Vector2(50, 50);
+        
+        thisEnemies.push(patrolScene.AddEntity({ tag: 'enemy', obj: newEnemy(), canMove: true, attachedSprite: 'enemy' }));
     }
 });
 
