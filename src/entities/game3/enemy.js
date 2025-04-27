0ac5aaeacc7abe153d0ef3ec86eb570668a647fd
FelipeGomeees <felipegomes.dsn@gmail.com>
2025-04-27 17:26:14 -0300

    feat: factory function changed the enemy to a factory function, allowing for a true random position on its instances

--- src/entities/game3/enemy.js
+++ src/entities/game3/enemy.js
@@ -6,16 +6,17 @@ import { AudioControl } from "../../modules/vortex.js";
 const size = new Vector2(30, 30);
 const bounds = new Vector2(800, 600);
 
-const position = Vector2.RandomWithin(bounds.Subtract(size));
-const direction = Vector2.RandomBetween(new Vector2(-1, 1), new Vector2(-1, 1))
-
-export const enemy = {
-    position: position,
-    size: size,
-    color: "#00FF00",
-    speed: 4,
-    direction: direction,
-};
+export function newEnemy() {
+    const position = Vector2.RandomWithin(bounds.Subtract(size));
+    const direction = Vector2.RandomBetween(new Vector2(-1, 1), new Vector2(-1, 1))
+    return {
+        position: position,
+        size: size,
+        color: "#00FF00",
+        speed: 4,
+        direction: direction,
+    }
+}
 
 export const enemyAI = (entity) => {
     const bounce = checkOutsideBounds(entity, new Vector2(RenderService.vw, RenderService.vh));
